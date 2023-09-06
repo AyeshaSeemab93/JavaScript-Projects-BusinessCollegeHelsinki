8d4a2d807413e09cd03409c013eeab2438fc1a88
AyeshaSeemab93 <ayeshaseemab2017@gmail.com>
2023-09-06 14:30:44 +0300

    start Onclick method from 0 not 1

--- 04-change-font/main.js
+++ 04-change-font/main.js
@@ -4,19 +4,21 @@ let fontbutton =  document.body.querySelector(".fontButton");
 
 
 
-let fontsize = [20, 24, 28, 32, 16]
+let fontsize = [20, 24, 28, 32,16]
 let index = 0;
 kokobutton.onclick= ()=>{
-  index= (index + 1)%fontsize.length; //to keep it in cylce index =1,2,3,4 and again 0
+
   let newsize = fontsize[index];
   list.style.fontSize = newsize + 'px';
+  index= (index + 1)%fontsize.length; //to keep it in cylce index =1,2,3,4 and again 0
 
 }
 
 let fontFamily = ["Roboto", "Mansalva", "Grenze", "Turret Road", "Courier New" ];
 fontbutton.onclick = () => {
-  //to keep going round and round:
-index = (index + 1) %fontFamily.length
+
 let newFont = fontFamily[index];
 list.style.fontFamily = newFont;
+  //to keep going round and round:
+  index = (index + 1) %fontFamily.length
 }
