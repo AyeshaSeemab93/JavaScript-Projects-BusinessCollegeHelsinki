ec9a6bfea2ebebc04d71541ece157155631664d6
AyeshaSeemab93 <ayeshaseemab2017@gmail.com>
2023-09-06 09:52:07 +0300

    Font Exercise

---
+++ 04-change-font/main.js
@@ -0,0 +1,21 @@
+let kokobutton = document.body.querySelector(".kokoButton");
+let list = document.body.querySelector("ol");
+let fontbutton =  document.body.querySelector(".fontButton");
+
+
+
+let fontsize = [20, 24, 28, 32, 16]
+let index = 0;
+kokobutton.onclick= ()=>{
+  index= (index + 1)%fontsize.length; //to keep it in cylce index =1,2,3,4 and again 0
+  let newsize = fontsize[index];
+  list.style.fontSize = newsize + 'px';
+
+}
+
+let fontFamily = ["Roboto", "Mansalva", "Grenze", "Turrent Road", "Courier New" ];
+fontbutton.onclick = () => {
+index = (index + 1) %fontFamily.length
+let newFont = fontFamily[index];
+list.style.fontFamily = newFont;
+}
